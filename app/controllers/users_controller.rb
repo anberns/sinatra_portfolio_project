e7c69432dec315c2f2c2e4d64fdc102b8807282c
Aaron Berns <anberns@gmail.com>
2019-02-02 09:45:56 -0500

    implement user controller routes for login, signup gets

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  #load signup form
+  get '/signup' do
+    if logged_in?
+      redirect to '/records'
+    else
+      erb :'users/signup'
+    end
+  end
+
+  #login form
+  get 'login' do 
+    if logged_in?
+      @user = current_user
+      redirect to '/records'
+    else
+      erb :'users/login'
+    end
+  end
+
+end
