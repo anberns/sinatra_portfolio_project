f7f4f6ee0c51d431190e5181bb7de72cc6f1cb79
Aaron Berns <anberns@gmail.com>
2019-02-02 08:22:25 -0500

    initial commit

---
+++ lib/record_collection.rb
@@ -0,0 +1,6 @@
+require "record_collection/version"
+
+module RecordCollection
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/record_collection/version.rb
@@ -0,0 +1,3 @@
+module RecordCollection
+  VERSION = "0.1.0"
+end
