7cff91d7a6dfc4c4011f5a1d83492e45c7f4f5e9
Aaron Berns <anberns@gmail.com>
2019-02-02 09:54:07 -0500

    implement post routes for user signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
     end
   end
 
+  #create new user
+  post '/signup' do 
+    if params["username"] == "" || params["email"] == "" || params["password"] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params["username"], email: params["email"], password_digest: params["password"])
+      session[:user_id] = @user.id
+      redirect '/records'
+    end
+
   #login form
   get '/login' do 
     if logged_in?
@@ -16,6 +26,17 @@ class UsersController < ApplicationController
       redirect to '/records'
     else
       erb :'users/login'
+    end
+  end
+
+  #login user
+  post '/login' do 
+    user = User.find_by(username: params["username"])
+    if user.authenticate(params["password"])
+      session[:user_id] = user.id
+      redirect '/records'
+    else 
+      redirect to '/'
     end
   end
 
