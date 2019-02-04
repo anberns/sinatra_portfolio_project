e331140eb7fa46fa0bb54336dfd647665bf88207
Aaron Berns <anberns@gmail.com>
2019-02-04 08:31:09 -0500

    add error handling to scraping function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,12 @@ class ApplicationController < Sinatra::Base
   end
 
   def find_album_info(artist, title)
-    html = open("https://www.discogs.com/search/?type=all&title=#{plusify(title)}&artist=#{plusify(artist)}&label=&track=&catno=&barcode=&anv=&format=&credit=&genre=&style=&country=&year=&submitter=&contributor=&matrix=&advanced=1")
+    begin
+      html = open("https://www.discogs.com/search/?type=all&title=#{plusify(title)}&artist=#{plusify(artist)}&label=&track=&catno=&barcode=&anv=&format=&credit=&genre=&style=&country=&year=&submitter=&contributor=&matrix=&advanced=1")
+    rescue
+      @message = "Something went wrong when downloading track information."
+      erb :'error'
+    end
     search_results = Nokogiri::HTML(html)
     release = search_results.css("div.shortcut_navigable")[0]
     url = release.css("a")[0].attribute("href").value
